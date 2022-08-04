1f46536cdd8db9eb16fcaa9151b78f6b3ea9456e
vishal <vishalpalode9899@gmail.com>
2022-08-04 16:58:22 +0530

    [vishalPalode] Added Frames handling test

---
+++ src/test/java/com/bridgelabz/selenium/Frames_Handling_Test.java
@@ -0,0 +1,70 @@
+package com.bridgelabz.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Frames_Handling_Test {
+@Test
+public void frames_usingIndex () throws InterruptedException{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("file:///C:/Users/Lenovo/Documents/page2.html");
+	//using index of the frame - [ int value] [ index of frames starts with zero]
+	driver.switchTo().frame(0);
+	driver.findElement(By.id("t1")).sendKeys("vishal");
+	driver.switchTo().defaultContent();
+	driver.findElement(By.id("t2")).sendKeys("patil");	
+}
+@Test
+public void frames_usingId () throws InterruptedException{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("file:///C:/Users/Lenovo/Documents/page2.html");
+	//using id attribute of the frame -string
+	driver.switchTo().frame("f1");
+	driver.findElement(By.id("t1")).sendKeys("vishal");
+	driver.switchTo().defaultContent();
+	driver.findElement(By.id("t2")).sendKeys("patil");
+}
+@Test
+public void frames_usingName () throws InterruptedException{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("file:///C:/Users/Lenovo/Documents/page2.html");
+	//using name attribute of the frame -string
+	driver.switchTo().frame("n1");
+	driver.findElement(By.id("t1")).sendKeys("vishal");
+	driver.switchTo().defaultContent();
+	driver.findElement(By.id("t2")).sendKeys("patil");
+}
+@Test
+public void frames_usingAddressFrame () throws InterruptedException{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("file:///C:/Users/Lenovo/Documents/page2.html");
+	//using address of the frame -webelement
+	WebElement f = driver.findElement(By.className("c1"));
+	driver.switchTo().frame(f);
+	driver.findElement(By.id("t1")).sendKeys("vishal");
+	driver.switchTo().defaultContent();
+	driver.findElement(By.id("t2")).sendKeys("patil");
+	//driver.close();
+}
+@Test
+public void frames_ThreeTextField() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("file:///C:/Users/Lenovo/Documents/page3.html");
+	// frames handling for three text field
+	//using ID attribute
+	driver.switchTo().frame("f1");
+	driver.findElement(By.id("t1")).sendKeys("vishal");
+	driver.switchTo().defaultContent();
+	driver.findElement(By.id("t2")).sendKeys("R");
+	driver.findElement(By.id("t3")).sendKeys("patil");
+
+}
+}
